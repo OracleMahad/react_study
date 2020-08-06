9e7970364b60056f7fde750ef521a5ce1ca1d754
mrnglory <mrnglory@mail.hongik.ac.kr>
2020-08-06 15:23:47 +0900

    MyName.js

--- hello-react/src/MyName.js
+++ hello-react/src/MyName.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class MyName extends React.Component {
-    render() {
-        return (
-            <div>
-                안녕하세요! 제 이름은 <b>{this.props.name}</b> 입니다.
-            </div>
-        )
-    }
-}
+const MyName = ({ name }) => {
+    return (
+        <div>
+            안녕하세요! 제 이름은 {name} 입니다.
+        </div>
+    );
+};
 
 export default MyName;
