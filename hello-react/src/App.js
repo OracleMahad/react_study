7048d4bff215a5cb7d4bff09352a96fef15eb26d
mrnglory <mrnglory@mail.hongik.ac.kr>
2020-08-06 14:44:57 +0900

    hello-react

---
+++ hello-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+        <div>
+            <h1>Hello React!</h1>
+            <h2>Welcome!</h2>
+        </div>
+        )
+    }
+}
+
+export default App;
