77bc99b7959780a19a1236505483d09cb25f7318
OracleMahad <61798131+OracleMahad@users.noreply.github.com>
2020-08-16 05:16:59 +0900

    commit

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,25 +1,84 @@
-import React from 'react';
-import PropTypes from "prop-types";
-
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css"
+// import PropTypes from "prop-types";
 
 class App extends React.Component{
-  state = {
-    count: 0
+  state ={
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    this.setState(current => ({count: current.count + 1 }));//{count: this.state.count + 1}
+  getMovies = async () => {
+    const {
+      data: { 
+        data: { movies } 
+      } 
+    }= await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json"
+    );
+    this.setState({movies, isLoading : false})//movies:movies
   };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1 }));//{count: this.state.count - 1}
-  };
-
+  componentDidMount() {
+    this.getMovies();
+  }
   render(){
-  return <div>
-    <h1>The nunber is {this.state.count}</h1>
-    <button onClick={this.add}>Add</button>
-    <button onClick={this.minus}>Minus</button>
-  </div>
+    const { isLoading, movies } = this.state;
+    return  (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div> 
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie 
+              key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+    </section>
+    );
   }
 }
+
+
+
+
+// componentDidMount() {
+//   setTimeout(()=> {
+//     this.setState({ isLoading: false });
+//   }, 6000);
+// }
+// className App extends React.Component{
+//   state = {
+//     count: 0
+//   };
+//   add = () => {
+//     this.setState(current => ({count: current.count + 1 }));//{count: this.state.count + 1}
+//   };
+//   minus = () => {
+//     this.setState(current => ({count: current.count - 1 }));//{count: this.state.count - 1}
+//   };
+
+//   // componentDidMount(){  }
+//   // componentDidUpdates(){  }
+//   // componentWillUnmount(){  }  Life cycle
+//   render(){
+//   return <div>
+//     <h1>The nunber is {this.state.count}</h1>
+//     <button onClick={this.add}>Add</button>
+//     <button onClick={this.minus}>Minus</button>
+//   </div>
+//   }
+// }
 
 export default App;
